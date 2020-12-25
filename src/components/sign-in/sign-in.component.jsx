2522707a648ccd9cf9cca350b1eb68af51214e49
Brian Robert Cajulis <brianrobertcajulis@yahoo.com>
2020-12-25 12:55:52 -0800

    made adjustment to the styles for custom button and sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -48,10 +48,12 @@ class SignIn extends Component {
             label='password'
           />
 
-          <CustomButton type='submit'>Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            Sign In With Google
-          </CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit'>Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign In With Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
