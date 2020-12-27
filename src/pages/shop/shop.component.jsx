5c01d63ec90a3d54536c23705f0c3f9e37d157d5
Brian Robert Cajulis <brianrobertcajulis@yahoo.com>
2020-12-27 14:03:10 -0800

    moved out directory and shop data into its own reducer and created respective selectors corresponding to the redux flow

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
-import CategoryPage from '../category/category.component';
+import CategoryPage from '../collection/collection.component';
 
 const ShopPage = ({ match }) => {
   return (
     <div className='shop-page'>
       <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CategoryPage} />
     </div>
   );
 };
